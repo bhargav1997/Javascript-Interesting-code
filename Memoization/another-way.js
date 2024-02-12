fb0a0619a61398f63a66b23ff615ddf1d3664f9c
Bhargav Suthar <sutharbhargav1997@gmail.com>
2024-02-12 01:22:53 -0500

    Update another-way.js

--- Memoization/another-way.js
+++ Memoization/another-way.js
@@ -14,3 +14,15 @@ function memoize(fn) {
                 }
     }
 }
+
+let callCount = 0;
+const memoizedFn = memoize(function (a, b) {
+ 	 callCount += 1;
+    return a + b;
+})
+
+memoizedFn(2, 3) // 5
+memoizedFn(2, 3) // 5
+
+console.log(callCount) // 1 
+
